3f09fcd024c6038aacd9f3031c3267a976f4fca2
Aleksander Bobryshev <aleksanderbobryshev@gmail.com>
2021-07-05 00:08:51 +0300

    Добавьте файлы проекта.

---
+++ homeWork6_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace homeWork6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userSurename;
+            string userTown;
+            string userHobby;
+            int userAge;
+
+            Console.WriteLine("Здравствуйте. Представьтесь");
+            Console.Write("Ваше имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Ваша фамилия: ");
+            userSurename = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш город проживания: ");
+            userTown = Console.ReadLine();
+            Console.Write("Ваше увлечение: ");
+            userHobby = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + userName + " " + userSurename + ", Вам " + userAge + " лет. Вы из города " + userTown + ". Ваше увлечение это " + userHobby);
+        }
+    }
+}
+
+
+
+
+       
